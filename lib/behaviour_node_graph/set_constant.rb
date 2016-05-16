f84a705cebe96b8ae2c7bfc254f61748c72fde13
Thomas Rogers <thomasrogers03@gmail.com>
2016-05-16 08:29:29 -0400

    Refactor SetConstant to use .define_simple_node

--- lib/behaviour_node_graph/set_constant.rb
+++ lib/behaviour_node_graph/set_constant.rb
@@ -1,10 +1,5 @@
 module BehaviourNodeGraph
-  class SetConstant < Struct.new(:target, :value)
-    include Node
-
-    def act
-      context.values[target] = value
-      super
-    end
+  SetConstant = define_simple_node(:target, :value) do
+    context.values[target] = value
   end
 end
