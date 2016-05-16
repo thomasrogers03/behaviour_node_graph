f1c86321d3ce556d80d299fdc597c5e0e38c924f
Thomas Rogers <thomasrogers03@gmail.com>
2016-05-16 18:17:48 -0400

    Create Counter node type

---
+++ lib/behaviour_node_graph/counter.rb
@@ -0,0 +1,5 @@
+module BehaviourNodeGraph
+  Counter = BehaviourNodeGraph.define_simple_node(:target, :increment) do
+    context.values[target] = (context.values[target] || 0) + increment
+  end
+end
---
+++ spec/classes/counter_spec.rb
@@ -0,0 +1,37 @@
+require 'rspec'
+
+module BehaviourNodeGraph
+  describe Counter do
+
+    let(:context) { Context.new }
+    let(:target) { Faker::Lorem.word }
+    let(:increment) { rand(1...1000) }
+    let(:initial_value) { nil }
+
+    subject { Counter.new_node(target, increment) }
+
+    before { context.values[target] = initial_value }
+
+    it { is_expected.to be_a_kind_of(Node) }
+
+    describe '#act' do
+      before do
+        subject.context = context
+        subject.act
+      end
+
+      it 'sets the current value of the target to the increment' do
+        expect(context.values[target]).to eq(increment)
+      end
+
+      context 'with a previously existing value' do
+        let(:initial_value) { rand(1...1000) }
+
+        it 'updates the target to the original value plus the increment' do
+          expect(context.values[target]).to eq(initial_value + increment)
+        end
+      end
+    end
+
+  end
+end
